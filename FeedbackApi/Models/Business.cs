b031860007793a494c9869235a2eadfefedc5097
Adegoke Temitope <adegoketemitope1909@gmail.com>
2021-11-11 04:27:47 -0500

    first commit

---
+++ FeedbackApi/Models/Business.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FeedbackApi.Models
+{
+  public class Business
+  {
+
+  }
+}
